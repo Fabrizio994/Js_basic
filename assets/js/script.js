ed8b66a5c77a171ee731fb20e34b44313be25506
Fabrizio Recchia <fabrizio643@hotmail.it>
2024-05-13 19:20:54 +0200

    creazione counter

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// contatore
+let counter = 0;
+
+// h1
+const h1Title = document.createElement("h1");
+h1Title.className = "title";
+h1Title.innerHTML = "Sono un counter";
+document.body.appendChild(h1Title);
+
+// div contatore
+const counterDiv = document.createElement("div");
+counterDiv.className = "counterDiv";
+counterDiv.innerHTML = counter;
+document.body.appendChild(counterDiv);
+
+//pulsante "decrementare"
+const decrementButton = document.createElement("button");
+decrementButton.className = "decrementBtn";
+decrementButton.innerHTML = "-";
+decrementButton.addEventListener("click", () => {
+  counter--;
+  counterDiv.innerHTML = counter;
+});
+document.body.appendChild(decrementButton);
+
+//pulsante "Reset"
+const resetButton = document.createElement("button");
+resetButton.className = "resetBtn";
+resetButton.innerHTML = "Reset";
+resetButton.addEventListener("click", () => {
+  counter = 0;
+  counterDiv.innerHTML = counter;
+});
+document.body.appendChild(resetButton);
+
+//pulsante "incrementare"
+const incrementButton = document.createElement("button");
+incrementButton.className = "incrementBtn";
+incrementButton.innerHTML = "+";
+incrementButton.addEventListener("click", () => {
+  counter++;
+  counterDiv.innerHTML = counter;
+});
+document.body.appendChild(incrementButton);
